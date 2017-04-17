c39f94aaf5ab5aebe9b548ea91403a1cd82fac5b
donhoellein <donhoellein@gmail.com>
2017-04-17 15:34:44 -0400

    Added input from JSON file

--- phonetic-reduce.js
+++ phonetic-reduce.js
@@ -18,17 +18,37 @@ var params = process.argv;
 console.log(params);
 
 var filename = params[3];
+var idx = filename.lastIndexOf('.') + 1;
+var fileExt = filename.slice(idx);
+//console.log('extension:', fileExt);
+
 var num = params[2];
 
 // Synchronous read
-var data = fs.readFileSync(filename);
-var dataStr = data.toString();
-var arStr = dataStr.split('\r\n');
-console.log(arStr);
+var data = fs.readFileSync(filename).toString();
+console.log("INPUT DATA:\r\n", data);
+
+var arStr = [];
+var dataJ = null;
+if (fileExt.toUpperCase() == 'JSON') {
+    dataJ = JSON.parse(data);
+    //console.log ("length: ", dataJ.length);
+    for(var ii=0; ii<dataJ.length; ii+=1) {
+        //console.log(dataJ[ii]);
+        arStr.push(dataJ[ii].str);
+    }
+}
+else {
+    //var dataStr = data.toString();
+    arStr = data.split('\r\n');
+}
+
+//console.log(arStr);
 
 var arProcessed = [];
 var phonetic = natural.Metaphone;
 
+console.log("OUTPUT DATA");
 for(var ii=0; ii<arStr.length; ii+=1) {
     if (arStr[ii] == '')
         continue;
@@ -43,7 +63,6 @@ for(var ii=0; ii<arStr.length; ii+=1) {
     
 var outStr = JSON.stringify(arProcessed);
 fs.writeFileSync(filename+".phon", outStr);
-
 
 //console.log(data);
 console.log("Exiting");
